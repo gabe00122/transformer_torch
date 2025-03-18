c6f6887ad2944082db4a5da17d7d5aeec2be3cc5
Gabriel Keith <gabriel@stem.ai>
2025-03-18 19:52:44 -0400

    cleanup

--- sentiment_lm_torch/model/transformer.py
+++ sentiment_lm_torch/model/transformer.py
@@ -7,54 +7,6 @@ from sentiment_lm_torch.model.attention import AttentionBlock
 from sentiment_lm_torch.model.embeddings import Embedder
 from sentiment_lm_torch.model.feed_forward import FFBlock, GLUBlock
 
-class GRUGatingUnit(torch.nn.Module):
-    """
-    Overview:
-        The GRUGatingUnit module implements the GRU gating mechanism used in the GTrXL model.
-    Interfaces:
-        ``__init__``, ``forward``
-    """
-
-    def __init__(self, input_dim: int, bg: float = 2.):
-        """
-        Overview:
-            Initialize the GRUGatingUnit module.
-        Arguments:
-            - input_dim (:obj:`int`): The dimensionality of the input.
-            - bg (:obj:`bg`): The gate bias. By setting bg > 0 we can explicitly initialize the gating mechanism to \
-                be close to the identity map. This can greatly improve the learning speed and stability since it \
-                initializes the agent close to a Markovian policy (ignore attention at the beginning).
-        """
-
-        super(GRUGatingUnit, self).__init__()
-        self.Wr = torch.nn.Linear(input_dim, input_dim, bias=False)
-        self.Ur = torch.nn.Linear(input_dim, input_dim, bias=False)
-        self.Wz = torch.nn.Linear(input_dim, input_dim, bias=False)
-        self.Uz = torch.nn.Linear(input_dim, input_dim, bias=False)
-        self.Wg = torch.nn.Linear(input_dim, input_dim, bias=False)
-        self.Ug = torch.nn.Linear(input_dim, input_dim, bias=False)
-        self.bg = nn.Parameter(torch.full([input_dim], bg))  # bias
-        self.sigmoid = torch.nn.Sigmoid()
-        self.tanh = torch.nn.Tanh()
-
-    def forward(self, x: torch.Tensor, y: torch.Tensor):
-        """
-        Overview:
-            Compute the output value using the GRU gating mechanism.
-        Arguments:
-            - x: (:obj:`torch.Tensor`): The first input tensor.
-            - y: (:obj:`torch.Tensor`): The second input tensor. \
-                x and y should have the same shape and their last dimension should match the input_dim.
-        Returns:
-            - g: (:obj:`torch.Tensor`): The output of the GRU gating mechanism. \
-                The shape of g matches the shapes of x and y.
-        """
-
-        r = self.sigmoid(self.Wr(y) + self.Ur(x))
-        z = self.sigmoid(self.Wz(y) + self.Uz(x) - self.bg)
-        h = self.tanh(self.Wg(y) + self.Ug(torch.mul(r, x)))  # element wise multiplication
-        g = torch.mul(1 - z, x) + torch.mul(z, h)
-        return g  # x.shape == y.shape == g.shape
 
 class TransformerLayer(nn.Module):
     def __init__(
@@ -83,19 +35,15 @@ class TransformerLayer(nn.Module):
         ff_block = GLUBlock if glu else FFBlock
         self.ffn = ff_block(d_model, ffn_size, activation=activation)
 
-        # self.attn_gate = GRUGatingUnit(d_model)
-        # self.ffn_gate = GRUGatingUnit(d_model)
 
     def forward(self, x: Tensor, positions: Tensor, block_mask: BlockMask | None = None) -> Tensor:
         attention_input = self.attention_norm(x)
         attention = self.attention(attention_input, positions, block_mask=block_mask)
-        x = x + attention# * self.attn_gate(x)
-        # x = self.attn_gate(x, attention)
+        x = x + attention
 
         feed_forward_input = self.ffn_norm(x)
         feed_forward = self.ffn(feed_forward_input)
-        x = x + feed_forward# * self.ffn_gate(x)
-        # x = self.ffn_gate(x, feed_forward)
+        x = x + feed_forward
 
         return x
 
@@ -157,6 +105,5 @@ class TransformerModel(nn.Module):
 
         x = self.output_norm(x)
         x = self.embedder(x, decode=True)
-        # x = jnp.asarray(x, dtype=jnp.float32)
 
         return x
